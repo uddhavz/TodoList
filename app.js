5834226185b9f20b0be17c19acf3c85634e99f5a
Uddhav Zambare <zambare.u@northeastern.edu>
2023-06-20 17:53:02 -0400

    Added CSS and refactored code

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+// console.log(date.exports);
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 const port = 3000;
+const items = ["Buy Grocery","Cook Food"];
+const workItems = [];
 
-
-app.get("/", function(req, res){
-    // res.send("Hello");
-
-
-var today  =  new Date();
-
-if(today.getDay() === 6 || today.getDay() === 0){
-    res.send("<h1>Yay it's the weekend!</h1>");
-}else{
-    // res.set("Content-Type", "text/html");
-    res.sendFile(__dirname+"/index.html");
-    // res.send();
-}
-
+app.get("/", function (req, res) {
+   let day = date.getDate();
+   res.render("list", { listTitle: day, newListItem: items });
 });
 
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
 
-app.listen(port,function(){
-    console.log("Server started on "+ port);
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item)
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work", newListItem: workItems });
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item)
+    res.redirect("/work");
+});
+
+app.listen(port, function () {
+    console.log("Server started on " + port);
 });
